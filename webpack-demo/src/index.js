66533fb93ee7f47848abe8793720d866e7f66bff
3115988782@qq.com <3115988782@qq.com>
2021-06-17 22:35:45 +0800

    1

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,19 +1,24 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './icon.jpg';
-
+// import './style.css'
+// import Icon from './icon.jpg';
+// import Data from './data.xml';
+import printMe from './print.js';
 function component() {
   var element = document.createElement('div');
+   var btn = document.createElement('button');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack??'], ' ');
-  element.classList.add('hello');
-  var myIcon = new Image();
-   myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+ // element.classList.add('hello');
+  //var myIcon = new Image();
+  // myIcon.src = Icon;
 
-   element.appendChild(myIcon);
+  // element.appendChild(myIcon);
+    element.appendChild(btn);
 
-
+   console.log(Data);
   return element;
 }
 
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,26 +1,31 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  entry:{
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
-  }
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/,
+  //       use: [
+  //         'style-loader',
+  //         'css-loader'
+  //       ]
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|gif)$/,
+  //       use: [
+  //         'file-loader'
+  //       ]
+  //     }
+  //   ]
+  // }
 };
